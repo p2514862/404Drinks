21c0eb1bc9d7452a57671192df9d0b5177e16024
Fatimah Badin <P2514863@my365.dmu.ac.uk>
2021-02-06 20:40:44 +0000

    intance only

---
+++ Testing2/tstCustomers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstCustomers
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //Create an instance of the class we want to create 
+            clsCustomers ACustomer = new clsCustomers();
+
+            //Test to see that it exists
+            Assert.IsNotNull(ACustomer);
+        }
+
+
+    }
+}
