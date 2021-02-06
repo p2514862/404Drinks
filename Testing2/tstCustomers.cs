657eb2ff5c2fe82bb382f732495ffd64d233789c
Fatimah Badin <P2514863@my365.dmu.ac.uk>
2021-02-06 23:06:31 +0000

    Properties and presentation layer

--- Testing2/tstCustomers.cs
+++ Testing2/tstCustomers.cs
@@ -18,6 +18,31 @@ namespace Testing2
         }
 
         [TestMethod]
+        public void LivesInUKCustomerOK()
+        {
+            //Create an instance of the class we want ot create
+            clsCustomers ACustomer = new clsCustomers();
+
+            //Create some test data to assign to the property 
+            Boolean TestData = true;
+
+            //Assign the data to the property
+            ACustomer.LivesInUK = TestData;
+
+            //Test to see the two values are the same
+            Assert.AreEqual(ACustomer.LivesInUK, TestData);
+        }
+
+        [TestMethod]
+        public void DateOfBirthCustomerOK()
+        {
+            clsCustomers ACustomer = new clsCustomers();
+            DateTime TestData = DateTime.Now.Date;
+            ACustomer.DateOfBirth = TestData;
+            Assert.AreEqual(ACustomer.DateOfBirth, TestData);
+        }
+
+        [TestMethod]
         public void CustomerIDCustomerOK()
         {
             clsCustomers ACustomer = new clsCustomers();
@@ -34,6 +59,34 @@ namespace Testing2
             ACustomer.CustomerName = TestData;
             Assert.AreEqual(ACustomer.CustomerName, TestData);
         }
+
+        [TestMethod]
+        public void CustomerEmailCustomerOK()
+        {
+            clsCustomers ACustomer = new clsCustomers();
+            String TestData = "fatimahbadin@gmail.com";
+            ACustomer.CustomerEmail = TestData;
+            Assert.AreEqual(ACustomer.CustomerEmail, TestData);
+        }
+
+        [TestMethod]
+        public void PasswordCustomerOK()
+        {
+            clsCustomers ACustomer = new clsCustomers();
+            String TestData = "Fatimah123";
+            ACustomer.Password = TestData;
+            Assert.AreEqual(ACustomer.Password, TestData);
+        }
+
+        [TestMethod]
+        public void CustomerAddressCustomerOK()
+        {
+            clsCustomers ACustomer = new clsCustomers();
+            String TestData = "123 Green Street LE4 6HB";
+            ACustomer.CustomerAddress = TestData;
+            Assert.AreEqual(ACustomer.CustomerAddress, TestData);
+        }
+
 
 
     }
