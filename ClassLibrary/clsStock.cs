b8f87476804b4312c8fe41204142cedb56ba34e0
Kajal Rana <P2514865@my365.dmu.ac.uk>
2021-02-10 13:19:16 +0000

    Added code for the properties table for Stock management

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
+        public int StockNo { get; set; }
+        public string DrinkType { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+        public DateTime DateRecieved { get; set; }
+        public bool Availability { get; set; }
     }
 }
