4fc0b49051b85886da4858abd138ba41dc1a00e3
Darshanika <78047950+Darshanika@users.noreply.github.com>
2021-02-11 18:32:34 +0000

    I have added simple interface for my table, chnaged code in tstStaff for monthly salary.

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -6,10 +6,11 @@ namespace ClassLibrary
     {
         public int StaffID { get; set; }
         public string FullName { get; set; }
-        public DateTime DOB { get; set; }
         public string Email { get; set; }
         public bool OnHoliday { get; set; }
         public int BranchID { get; set; }
-        public int MonthlySalary { get; set; }
+        
+        public decimal MonthlySalary { get; set; }
+        public DateTime DOB { get; set; }
     }
 }
