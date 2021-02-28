61ff4afef7229b46d7a8026d9a1feba214e91f15
Mohmed-Ayaz Suleman <P2514862@my365.dmu.ac.uk>
2021-02-28 16:29:42 +0000

    some progress on creating collection class doc

---
+++ ClassLibrary/clsOrderCollection.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsOrderCollection
+    {
+        public List<clsOrder> OrderList { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ Testing1/tstOrderCollection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstOrderCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            Assert.IsNotNull(AllOrders);
+        }
+        [TestMethod]
+        public void OrdersListOK()
+        {
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            List<clsOrder> TestList = new List<clsOrder>();
+            clsOrder TestItem = new clsOrder();
+            TestItem.OrderID = 1;
+            TestItem.CustomerID = 1;
+            TestItem.ProductID = 1;
+            TestItem.ShippingCompany = "Royal Mail";
+            TestItem.DateOfPurchase = Convert.ToDateTime("21/01/2021");
+            TestItem.Over18 = true;
+            TestItem.TotalPrice = 6;
+
+            TestList.Add(TestItem);
+            AllOrders.OrderList = TestList;
+            Assert.AreEqual(AllOrders.OrderList, TestList);
+        }
+        [TestMethod]
+        public void CountPropertyOK()
+        {
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            Int32 SomeCount = 0;
+            AllOrders.Count = SomeCount;
+            Assert.AreEqual(AllOrders.Count, SomeCount);
+        }
+    }
+}
