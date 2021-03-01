4b2b4cb5432a0ad4b25bf852a4d8a84d481c16de
Mohmed-Ayaz Suleman <P2514862@my365.dmu.ac.uk>
2021-03-01 03:19:25 +0000

    Almost complete week 22 both docs

--- ClassLibrary/clsOrderCollection.cs
+++ ClassLibrary/clsOrderCollection.cs
@@ -1,10 +1,62 @@
 ﻿using System.Collections.Generic;
+using System;
+using ClassLibrary;
 
 namespace ClassLibrary
 {
     public class clsOrderCollection
     {
-        public List<clsOrder> OrderList { get; set; }
-        public int Count { get; set; }
+
+        List<clsOrder> mOrderList = new List<clsOrder>();
+
+        public List<clsOrder> OrderList
+        {
+            get
+            {
+                return mOrderList;
+            }
+            set
+            {
+                mOrderList = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return mOrderList.Count;
+            }
+            set
+            {
+                //worry about this later////////////////
+            }
+        }
+        public clsOrder ThisOrder { get; set; }
+
+        public clsOrderCollection()
+        {
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblOrder_SelectAll");
+            RecordCount = DB.Count;
+            while (Index < RecordCount)
+            {
+                clsOrder AnOrder = new clsOrder();
+                AnOrder.OrderID = Convert.ToInt32(DB.DataTable.Rows[Index]["OrderID"]);
+                AnOrder.ProductID = Convert.ToInt32(DB.DataTable.Rows[Index]["ProductID"]);
+                AnOrder.CustomerID = Convert.ToInt32(DB.DataTable.Rows[Index]["CustomerID"]);
+                AnOrder.DateOfPurchase = Convert.ToDateTime(DB.DataTable.Rows[Index]["DateOfPurchase"]);
+                AnOrder.ShippingCompany = Convert.ToString(DB.DataTable.Rows[Index]["ShippingCompany"]);
+                AnOrder.Over18 = Convert.ToBoolean(DB.DataTable.Rows[Index]["Over18"]);
+                AnOrder.TotalPrice = Convert.ToDecimal(DB.DataTable.Rows[Index]["TotalPrice"]);
+                mOrderList.Add(AnOrder);
+
+                Index++;
+            }
+        }
     }
+    
 }
--- Testing1/tstOrderCollection.cs
+++ Testing1/tstOrderCollection.cs
@@ -32,6 +32,7 @@ namespace Testing1
             AllOrders.OrderList = TestList;
             Assert.AreEqual(AllOrders.OrderList, TestList);
         }
+        /*
         [TestMethod]
         public void CountPropertyOK()
         {
@@ -40,5 +41,43 @@ namespace Testing1
             AllOrders.Count = SomeCount;
             Assert.AreEqual(AllOrders.Count, SomeCount);
         }
+        */
+        [TestMethod]
+        public void ThisOrderPropertyOK()
+        {
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            clsOrder TestOrder = new clsOrder();
+            TestOrder.OrderID = 1;
+            TestOrder.CustomerID = 1;
+            TestOrder.ProductID = 1;
+            TestOrder.ShippingCompany = "Royal Mail";
+            TestOrder.DateOfPurchase = Convert.ToDateTime("21/01/2021");
+            TestOrder.Over18 = true;
+            TestOrder.TotalPrice = 6;
+
+            AllOrders.ThisOrder = TestOrder;
+            Assert.AreEqual(AllOrders.ThisOrder, TestOrder);
+        }
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            List<clsOrder> TestList = new List<clsOrder>();
+            clsOrder TestItem = new clsOrder();
+
+            TestItem.OrderID = 1;
+            TestItem.CustomerID = 1;
+            TestItem.ProductID = 1;
+            TestItem.ShippingCompany = "Royal Mail";
+            TestItem.DateOfPurchase = Convert.ToDateTime("21/01/2021");
+            TestItem.Over18 = true;
+            TestItem.TotalPrice = 6;
+
+            TestList.Add(TestItem);
+            AllOrders.OrderList = TestList;
+            Assert.AreEqual(AllOrders.Count, TestList.Count);
+
+        }
+       
     }
 }
