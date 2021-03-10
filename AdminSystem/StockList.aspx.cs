5c91901c7e569318e2ff17d98dfd7c04faec03eb
Kajal Rana <P2514865@my365.dmu.ac.uk>
2021-03-10 12:59:58 +0000

    Created the collection class for the presentation layer

--- AdminSystem/StockList.aspx.cs
+++ AdminSystem/StockList.aspx.cs
@@ -4,11 +4,28 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
-public partial class _1_List : System.Web.UI.Page
+public partial class _Default: System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (IsPostBack == false)
+        {
+            DisplayStock();
+        }
+    }
 
+    void DisplayStock()
+    {
+        //create an instance of the County collection
+        clsStockCollection Stock = new clsStockCollection();
+        lstStockList.DataSource = Stock.StockList;
+        //set the data source to the list of counties in the collection
+        lstStockList.DataValueField = "ProductID";
+        //set the data field to display
+        lstStockList.DataTextField = "DrinkType";
+        //bind the data to the list
+        lstStockList.DataBind();
     }
 }
