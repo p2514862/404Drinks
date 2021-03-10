204d0c37a9b3911349c6e376dbf4777e127c768a
Darshanika <78047950+Darshanika@users.noreply.github.com>
2021-03-10 13:47:41 +0000

    created collection class property presentation layer.

--- AdminSystem/StaffList.aspx.cs
+++ AdminSystem/StaffList.aspx.cs
@@ -4,11 +4,28 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
-public partial class _1_List : System.Web.UI.Page
+public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (IsPostBack == false)
+        {
+            DisplayStaffs();
+
+        }
+
+    }
+    void DisplayStaffs()
+    {
+        clsStaffCollection Staffs = new clsStaffCollection();
+        lstStaffList.DataSource = Staffs.StaffList;
+        lstStaffList.DataValueField = "StaffID";
+        lstStaffList.DataTextField = "Email";
+        lstStaffList.DataBind();
+
+
 
     }
 }
