89114a6032deb6a7db163c86976106d041841038
Fatimah Badin <P2514863@my365.dmu.ac.uk>
2021-03-10 13:31:27 +0000

    Collection class properties and presentation layer

---
+++ ClassLibrary/clsCustomerCollection.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace ClassLibrary
+{
+    public class clsCustomerCollection
+    {
+        List<clsCustomers> mCustomerList = new List<clsCustomers>();
+        public List<clsCustomers> CustomerList
+        {
+            get
+            {
+                return mCustomerList;
+            }
+            set
+            {
+                mCustomerList = value;
+            }
+
+        }
+        public clsCustomers ThisCustomer { get; set; }
+        public int Count
+        {
+            get
+            {
+                return mCustomerList.Count;
+            }
+            set
+            {
+                //do it later
+            }
+        }
+
+        //constructor
+        public clsCustomerCollection()
+        {
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblCustomers_SelectAll");
+            RecordCount = DB.Count;
+            while (Index < RecordCount)
+            {
+                clsCustomers ACustomer = new clsCustomers();
+                ACustomer.CustomerID = Convert.ToInt32(DB.DataTable.Rows[Index]["CustomerID"]);
+                ACustomer.CustomerName = Convert.ToString(DB.DataTable.Rows[Index]["CustomerName"]);
+                ACustomer.CustomerEmail = Convert.ToString(DB.DataTable.Rows[Index]["CustomerEmail"]);
+                ACustomer.Password = Convert.ToString(DB.DataTable.Rows[Index]["Password"]);
+                ACustomer.CustomerAddress = Convert.ToString(DB.DataTable.Rows[Index]["CustomerAddress"]);
+                ACustomer.DateOfBirth = Convert.ToDateTime(DB.DataTable.Rows[Index]["DateOfBirth"]);
+                ACustomer.LivesInUK = Convert.ToBoolean(DB.DataTable.Rows[Index]["LivesInUK"]);
+
+                mCustomerList.Add(ACustomer);
+                Index++;
+
+            }
+
+            clsCustomers TestItem = new clsCustomers();
+            TestItem.CustomerID = 1;
+            TestItem.CustomerName = "Fatimah Badin";
+            TestItem.CustomerEmail = "fatimah@gmail.com";
+            TestItem.Password = "fatimah123";
+            TestItem.CustomerAddress = "10 green street LE6 6BH";
+            TestItem.DateOfBirth = DateTime.Now.Date.AddYears(-20);
+            TestItem.LivesInUK = true;
+            mCustomerList.Add(TestItem);
+       
+            //re initialise the object for new data 
+            TestItem = new clsCustomers();
+            TestItem.CustomerID = 2;
+            TestItem.CustomerName = "Ayaz Suleman";
+            TestItem.CustomerEmail = "asuleman@hotmail.com";
+            TestItem.Password = "ayaz123";
+            TestItem.CustomerAddress = "165 park street LE9 7BS";
+            TestItem.DateOfBirth = DateTime.Now.Date.AddYears(-21);
+            TestItem.LivesInUK = true;
+            mCustomerList.Add(TestItem);
+
+        }
+    }
+}
---
+++ Testing2/tstCustomerCollection.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstCustomerCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            Assert.IsNotNull(AllCustomers);
+        }
+
+        [TestMethod]
+        public void CustomerListOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+
+            //test data - list of objects
+            List<clsCustomers> TestList = new List<clsCustomers>();
+
+            //add item to list - create item of test data 
+            clsCustomers TestItem = new clsCustomers();
+
+            //set properties
+            TestItem.CustomerID = 1;
+            TestItem.CustomerName = "Fatimah Badin";
+            TestItem.CustomerEmail = "fatimah@gmail.com";
+            TestItem.Password = "fatimah123";
+            TestItem.CustomerAddress = "10 green street LE6 6BH";
+            TestItem.DateOfBirth = DateTime.Now.Date.AddYears(-20);
+            TestItem.LivesInUK = true;
+
+            //add ietsm to test list
+            TestList.Add(TestItem);
+
+            //assign data to property
+            AllCustomers.CustomerList = TestList;
+
+            //test that the values are the same
+            Assert.AreEqual(AllCustomers.CustomerList, TestList); 
+        }
+
+        [TestMethod]
+        public void ThisCustomerPropertyOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            clsCustomers TestCustomer = new clsCustomers();
+            TestCustomer.CustomerID = 1;
+            TestCustomer.CustomerName = "Fatimah Badin";
+            TestCustomer.CustomerEmail = "fatimah@gmail.com";
+            TestCustomer.Password = "fatimah123";
+            TestCustomer.CustomerAddress = "10 green street LE6 6BH";
+            TestCustomer.DateOfBirth = DateTime.Now.Date.AddYears(-20);
+            TestCustomer.LivesInUK = true;
+            AllCustomers.ThisCustomer = TestCustomer;
+            Assert.AreEqual(AllCustomers.ThisCustomer, TestCustomer);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            List<clsCustomers> TestList = new List<clsCustomers>();
+            clsCustomers TestItem = new clsCustomers();
+            TestItem.CustomerID = 1;
+            TestItem.CustomerName = "Fatimah Badin";
+            TestItem.CustomerEmail = "fatimah@gmail.com";
+            TestItem.Password = "fatimah123";
+            TestItem.CustomerAddress = "10 green street LE6 6BH";
+            TestItem.DateOfBirth = DateTime.Now.Date.AddYears(-20);
+            TestItem.LivesInUK = true;
+            TestList.Add(TestItem);
+            AllCustomers.CustomerList = TestList;
+            Assert.AreEqual(AllCustomers.Count, TestList.Count);
+        }
+
+    }
+}
