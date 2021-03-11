290871e3f528c3e7e4a3d2a2764c07013f6f155d
razan <razanalm070@gmail.com>
2021-03-11 10:32:26 +0000

    Done the List presentation layer.

--- AdminSystem/BranchList.aspx.cs
+++ AdminSystem/BranchList.aspx.cs
@@ -4,11 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
-public partial class _1_List : System.Web.UI.Page
+public partial class BranchList : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //if this is the first time the page is displayed
+        if (IsPostBack == false)
+        {
+            //update the list box
+            DisplayBranches();
+        }
 
+    }
+    public void DisplayBranches()
+    {
+        //create an instance of the County Collection 
+        clsBranchCollection Branches = new clsBranchCollection();
+
+        //set the data source to the list of counties in the collection
+        lstBranchList.DataSource = Branches.BranchList;
+        //set the name of the primary key
+        lstBranchList.DataValueField = "BranchId";
+        //set the data feild to display 
+        lstBranchList.DataTextField = "ProductId";
+        //bind the data to the list 
+        lstBranchList.DataBind();
     }
 }
---
+++ Testing5/tstBranchCollection.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Testing5
+{
+    [TestClass]
+    public class tstBranchCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create 
+            clsBranchCollection AllBranches = new clsBranchCollection();
+            //test to see that exist 
+            Assert.IsNotNull(AllBranches);
+        }
+
+        [TestMethod]
+
+        public void BranchListOK()
+        {
+            //create an instance of the class we want to create 
+            clsBranchCollection AllBranches = new clsBranchCollection();
+            //create some test data to assign to the branch 
+            //in this case the data needs to be a list of objects
+            List<clsBranch> TestList = new List<clsBranch>();
+            //add an item to the list 
+            //create the item of test data 
+            clsBranch TestItem = new clsBranch();
+            //set its properties
+            TestItem.IsStaffAvailable = true;
+            TestItem.BranchId = 1;
+            TestItem.StaffId = 1;
+            TestItem.ProductId = 21;
+            TestItem.BranchStartDate = DateTime.Now.Date;
+            TestItem.BranchDescription = "Juice";
+            //add the item to the test list 
+            TestList.Add(TestItem);
+            //assign the data to the branch
+            AllBranches.BranchList = TestList;
+            //test to see that the two values are the same 
+            Assert.AreEqual(AllBranches.BranchList, TestList);
+        }
+
+
+
+        [TestMethod]
+
+        public void ThisBranchDataOK()
+        {
+            //create an instance of the class we want to create 
+            clsBranchCollection AllBranches = new clsBranchCollection();
+            //create some test data to assign to the property 
+            clsBranch TestBranch = new clsBranch();
+            //set the data of the test object
+            TestBranch.IsStaffAvailable = true;
+            TestBranch.BranchId = 1;
+            TestBranch.StaffId = 1;
+            TestBranch.ProductId = 21;
+            TestBranch.BranchStartDate = DateTime.Now.Date;
+            TestBranch.BranchDescription = "Juice";
+            //assign the data to the branch 
+            AllBranches.ThisBranch = TestBranch;
+            //test to see that the values are the same 
+            Assert.AreEqual(AllBranches.ThisBranch, TestBranch);
+        }
+
+        [TestMethod]
+
+        public void ListAndCountOK()
+        {
+            //create an instance of the class we want to create 
+            clsBranchCollection AllBranches = new clsBranchCollection();
+            //create some test data to assign to the branch
+            //in this case the data needs to be a list of objects 
+            List<clsBranch> TestList = new List<clsBranch>();
+            //add an item to the list 
+            //create the itme of test data 
+            clsBranch TestItem = new clsBranch();
+            //set its data 
+            TestItem.IsStaffAvailable = true;
+            TestItem.ProductId = 21;
+            TestItem.StaffId = 1;
+            TestItem.BranchId = 1;
+            TestItem.BranchStartDate = DateTime.Now.Date;
+            TestItem.BranchDescription = "Juice";
+            //add the item to the test list 
+            TestList.Add(TestItem);
+            //assign the data to the branch 
+            AllBranches.BranchList = TestList;
+            //test to see that values are the same 
+            Assert.AreEqual(AllBranches.Count, TestList.Count);
+
+        }
+
+    }
+}
